ec9b221889c10c71d4c9dcce29be01ff6daff23d
Weslen Anjos <weslen_anjos@outlook.com>
2024-09-07 17:49:41 -0300

    ATT de produção

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { bisseccao } = require('./controllers/equacoes')
+const { bisseccao } = require('./controllers/biseccaoController');
+const  {falsaPosicao}  = require('./controllers/falsaPosicaoController')
 
 // O Router é usado para modularizar e organizar as rotas
 const rotas = express.Router();
 
 // Rotas para os metodos
-rotas.post('/metodos', bisseccao );
+rotas.post('/bisseccao', bisseccao );
+rotas.post('/fp', falsaPosicao)
 
 module.exports = rotas;
