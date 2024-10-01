15a4d8542fb29c5b3c1e8b630c554f308df0aa9f
Weslen Anjos <weslen_anjos@outlook.com>
2024-09-30 23:42:43 -0300

    Comentando o cors para debbug

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ const app = express();
 // Middlewares globais
 
 // Configurar CORS com permissões específicas
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://numericsolve.vercel.app'); // Substitua pela origem do seu front-end
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true'); // Permitir envio de cookies
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
-});
+});*/
 
 app.use(express.json());  // Para aceitar JSON no corpo da requisição
 app.use(cookieParser());  // Para analisar cookies
